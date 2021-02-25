4903c5afc699606ab5553ec06d527f2c79ff9086
vsahinid <vsahinid@alumni.cmu.edu>
2021-02-25 13:01:59 -0600

    Added pusher

--- app/Message.php
+++ app/Message.php
@@ -6,5 +6,10 @@ use Illuminate\Database\Eloquent\Model;
 
 class Message extends Model
 {
-   protected $guarded = [];
+    protected $guarded = [];
+
+    public function fromContact()
+    {
+        return $this->hasOne(User::class, 'id', 'from');
+    }
 }
